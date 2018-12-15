3c1174fdbbacc9a8f50599a7a9e0cbc26bf00f02
Gunter Grodotzki <gunter@grodotzki.co.za>
2018-12-15 16:19:51 +0200

    add sig support (#4)

--- src/NINEJKH/GPGkeyinfo/Records/Sig.php
+++ src/NINEJKH/GPGkeyinfo/Records/Sig.php
@@ -1,1 +1,16 @@
 <?php
+
+/**
+ * This file is part of the NINEJKH/php-gpg-keyinfo library.
+ *
+ * (c) 9JKH (Pty) Ltd. <dev@9jkh.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace NINEJKH\GPGkeyinfo\Records;
+
+class Sig extends AbstractRecord
+{
+}
